dc8f28d08ed785c2bf4fde89eb48bb77bdb77500
Alexandre <alexmcs@gmail.com>
2023-03-14 08:29:19 -0300

    Implementando SignOut

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -9,7 +9,7 @@
       </RouterLink>
     </nav>
 
-    <footer>
+    <footer v-if="this.$cookies.get('accessToken') == null">
       <div class="footer-content">
         <RouterLink to="/signup">
           <button class="btn btn-primary btn-sm sign">Cadastrar</button>
@@ -32,11 +32,39 @@
         </RouterLink>
       </div>
     </footer>
+    <footer v-else>
+      <div class="footer-content">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          fill="#FFF"
+          class="bi bi-person-bounding-box user-svg"
+          viewBox="0 0 16 16"
+        >
+          <path
+            d="M1.5 1a.5.5 0 0 0-.5.5v3a.5.5 0 0 1-1 0v-3A1.5 1.5 0 0 1 1.5 0h3a.5.5 0 0 1 0 1h-3zM11 .5a.5.5 0 0 1 .5-.5h3A1.5 1.5 0 0 1 16 1.5v3a.5.5 0 0 1-1 0v-3a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 1-.5-.5zM.5 11a.5.5 0 0 1 .5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 1 0 1h-3A1.5 1.5 0 0 1 0 14.5v-3a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v3a1.5 1.5 0 0 1-1.5 1.5h-3a.5.5 0 0 1 0-1h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 1 .5-.5z"
+          />
+          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm8-9a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+        </svg>
+        <p>Olá, usuário!</p>
+        <a v-on:click="signOut()">Sair</a>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    signOut() {
+      this.$cookies.remove("accessToken");
+      this.$cookies.remove("refreshToken");
+      console.log("Deslogado")
+      console.log(this.$cookies.get('accessToken'))
+    },
+  },
+};
 </script>
 
 <style scoped>
